f1839b3d51eef3a904c86eaa9bcff23d32d9030e
TayKK <TayKK@users.noreply.github.com>
2021-02-28 23:06:16 +0800

    Updated POST Request
    - Changed links:
    4) www.mediacorp.sg
    5) www.straitstimes.com
    
    - Added POST Requests for all links
    - Updated Menu Output  with new links
    
    *Compiled with GCC 10.2.1 and wolfssl 4.7.0
    Kindly recompile ./testssl again thank you

--- ICT2205_Code.h
+++ ICT2205_Code.h
@@ -4,26 +4,24 @@
 #include <wolfssl/openssl/bio.h>
 #include <wolfssl/wolfcrypt/rsa.h>
 
-#define CERT_FILE_CA "./cert/USERTrustRSACertificationAuthority.pem" //Which website CA cert is this 
-#define CERT_FILE_CA2 "./cert/DigiCertGlobalRootCA.pem" //Which website CA cert is  this 
-#define CERT_FILE_CA3 "./cert/AmazonCA.pem" //Which website CA cert is  this 
-#define WEBSITE_MENU "Please choose a number according to the options to retrieve certificates\n1) www.sec-consult.com\n2) www.zara.com\n3) www.uniqlo.com\n4) app.mediafire.com\n5) www.wetransfer.com\n0) Exit\nOption: " //Starting menu for user
+#define CERT_FILE_CA "./cert/USERTrustRSACertificationAuthority.pem"                                                                                                                                                         //Which website CA cert is this
+#define CERT_FILE_CA2 "./cert/DigiCertGlobalRootCA.pem"                                                                                                                                                                      //Which website CA cert is  this
+#define CERT_FILE_CA3 "./cert/AmazonCA.pem"                                                                                                                                                                                  //Which website CA cert is  this
+#define WEBSITE_MENU "Please choose a number according to the options to retrieve certificates\n1) www.sec-consult.com\n2) www.zara.com\n3) www.uniqlo.com\n4) www.mediacorp.sg\n5) www.straitstimes.com\n0) Exit\nOption: " //Starting menu for user
 #define SC_MENU "Please choose a number accordingly. Option 1 & 2 are sample requests, to prove the script is working.\n1) GET - Homepage\n2) POST - Search function\n3) Custom request (Manually enter request)\n0) Return to main menu\nOption: "
-#define ZARA_MENU "Please choose a number accordingly. Option 1 & 2 are sample requests, to prove the script is working.\n1) GET - Homepage\n2) POST - Add to cart function\n3) Custom request (Manually enter request)\n0) Return to main menu\nOption: "
-#define UNI_MENU "Please choose a number accordingly. Option 1 & 2 are sample requests, to prove the script is working.\n1) GET - Homepage\n2) POST - Add to cart function\n3) Custom request (Manually enter request)\n0) Return to main menu\nOption: "
-//modify
-#define MF_MENU "Please choose a number accordingly. Option 1 & 2 are sample requests, to prove the script is working.\n1) GET - Homepage\n2) POST - Add to cart function\n3) Custom request (Manually enter request)\n0) Return to main menu\nOption: "
-//Modify
-#define WT_MENU "Please choose a number accordingly. Option 1 & 2 are sample requests, to prove the script is working.\n1) GET - Homepage\n2) POST - Add to cart function\n3) Custom request (Manually enter request)\n0) Return to main menu\nOption: "
+#define ZARA_MENU "Please choose a number accordingly. Option 1 & 2 are sample requests, to prove the script is working.\n1) GET - Homepage\n2) POST - Search function\n3) Custom request (Manually enter request)\n0) Return to main menu\nOption: "
+#define UNI_MENU "Please choose a number accordingly. Option 1 & 2 are sample requests, to prove the script is working.\n1) GET - Homepage\n2) POST - Search function\n3) Custom request (Manually enter request)\n0) Return to main menu\nOption: "
+#define MF_MENU "Please choose a number accordingly. Option 1 & 2 are sample requests, to prove the script is working.\n1) GET - Homepage\n2) POST - Search function\n3) Custom request (Manually enter request)\n0) Return to main menu\nOption: "
+#define WT_MENU "Please choose a number accordingly. Option 1 & 2 are sample requests, to prove the script is working.\n1) GET - Homepage\n2) POST - Search function\n3) Custom request (Manually enter request)\n0) Return to main menu\nOption: "
 
 // To create a socket & TCP-connect to server
 int create_socket(char[], BIO *);
 int program_cert(int num_option);
-int free_structures(WOLFSSL* ssl, int server, WOLFSSL_X509* servercert, WOLFSSL_CTX* ctx);
-void catchar(char* main, char c);
-char* replace_newline(char* buff);
-char* retr_request(int n, char* target);
-char* custom_request();
-int process_request(int n, char* target, WOLFSSL* ssl);
+int free_structures(WOLFSSL *ssl, int server, WOLFSSL_X509 *servercert, WOLFSSL_CTX *ctx);
+void catchar(char *main, char c);
+char *replace_newline(char *buff);
+char *retr_request(int n, char *target);
+char *custom_request();
+int process_request(int n, char *target, WOLFSSL *ssl);
 int program_cert(int num_option);
-char* retr_menu(int n);
+char *retr_menu(int n);
